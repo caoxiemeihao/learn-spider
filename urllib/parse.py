0a47e677000492de92fc741a8ed51c2ef9dd3189
草鞋没号 <308487730@qq.com>
2019-12-29 21:10:29 +0800

    urlparse、urlsplit

--- urllib/parse.py
+++ urllib/parse.py
@@ -9,5 +9,15 @@ def _urlencode():
     print(result2)
 
 
+def _urlparse():
+    url = 'https://www.baidu.com/baidu?&ie=utf-8&word=node.js'
+    # result = parse.urlparse(url)
+    # 'https://www.baidu.com/baidu;params?&ie=utf-8&word=node.js'
+    result = parse.urlsplit(url)  # 比 urlparse 少了一个 params
+    print(result)
+
+
 if __name__ == '__main__':
     _urlencode()
+    ...
+    _urlparse()
